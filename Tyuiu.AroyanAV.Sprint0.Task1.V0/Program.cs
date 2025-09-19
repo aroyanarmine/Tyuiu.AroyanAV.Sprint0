62bea1b8c0a031ddf5b5c506f11ea2d12bd22cc8
aroyanarmine <aroanarmine068@gmail.com>
2025-09-19 20:10:41 +0500

    Task1

---
+++ Tyuiu.AroyanAV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string name = "Армине";
+int age = 19;
+Console.WriteLine("Здраствуйте меня зовут " + name + " мне " + age + " лет!");
+Console.ReadKey();
