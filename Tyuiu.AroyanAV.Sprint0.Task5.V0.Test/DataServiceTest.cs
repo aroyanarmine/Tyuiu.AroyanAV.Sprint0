88e1720847c7aafb4397e137c8aa992a3823242f
aroyanarmine <aroanarmine068@gmail.com>
2025-09-20 00:08:44 +0500

    Task5

---
+++ Tyuiu.AroyanAV.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.AroyanAV.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.AroyanAV.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ChekedAddition()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+
+        [TestMethod]
+        public void ChekedSubtractionVaild()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+
+        [TestMethod]
+        public void ChekedMultipplicationVaild()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+
+        [TestMethod]
+        public void ChekedDivisionVaild()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
