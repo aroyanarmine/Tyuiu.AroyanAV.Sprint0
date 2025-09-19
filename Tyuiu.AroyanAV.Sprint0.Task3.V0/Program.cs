317922d31c64b7438a18d18670ea3191a4b85e0f
aroyanarmine <aroanarmine068@gmail.com>
2025-09-19 22:23:51 +0500

    Task3

---
+++ Tyuiu.AroyanAV.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Tyuiu.AroyanAV.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.AroyanAV.Sprint0.Task3.V0
+
+{ 
+   class Program
+   {
+       static void Main(string[] args)
+       {
+
+           Console.WriteLine(DataService.Sum(7, 7));
+           Console.ReadLine();
+        }
+   }
+
+}
