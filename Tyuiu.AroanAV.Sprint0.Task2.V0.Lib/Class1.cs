d35f01e3bc97ec62a16cc9241370454c6f8cfd9e
Vacho <Vacho@DESKTOP-LL27I8L>
2025-09-19 12:16:50 +0500

    Add project files.

---
+++ Tyuiu.AroanAV.Sprint0.Task2.V0.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.AroanAV.Sprint0.Task2.V0.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.AroyanAV.Sprint0.Task2.V0.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.AroyanAV.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
